b57cca92d6944e889f312c9f2a9b5adbd4b31f54
Deepak Kumar <d1612k@gmail.com>
2019-08-29 23:44:55 +0530

    linked list push method

---
+++ single_linked_list.js
@@ -0,0 +1,33 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+const list = new SinglyLinkedList();
+console.log(list.push("Hello"));
+console.log(list.push("World"));
+console.log(list.push("!"));
+console.log(list.push("!"));
