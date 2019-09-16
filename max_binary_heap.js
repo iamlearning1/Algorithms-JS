7dc177492f6151ccddaf5a949f154a6d15d2f105
Deepak Kumar <d1612k@gmail.com>
2019-09-16 21:34:18 +0530

    extract max max_binary_heap

--- max_binary_heap.js
+++ max_binary_heap.js
@@ -21,6 +21,51 @@ class MaxBinaryHeap {
 
     return this;
   }
+
+  remove() {
+    const max = this.data[0];
+    const end = this.data.pop();
+
+    if (this.data.length > 0) {
+      this.data[0] = end;
+      let index = 0;
+      const length = this.data.length;
+      const element = this.data[0];
+
+      while (true) {
+        const leftChildIndex = 2 * index + 1;
+        const rightChildIndex = 2 * index + 2;
+        let leftChild;
+        let rightChild;
+        let swap = null;
+
+        if (leftChildIndex < length) {
+          leftChild = this.data[leftChildIndex];
+          if (leftChild > element) {
+            swap = leftChildIndex;
+          }
+        }
+
+        if (rightChildIndex < length) {
+          rightChild = this.data[rightChildIndex];
+          if (
+            (!swap && rightChild > element) ||
+            (swap && rightChild > leftChild)
+          ) {
+            swap = rightChildIndex;
+          }
+        }
+
+        if (!swap) break;
+
+        this.data[index] = this.data[swap];
+        this.data[swap] = element;
+        index = swap;
+      }
+    }
+
+    return max;
+  }
 }
 
 const heap = new MaxBinaryHeap();
@@ -29,9 +74,10 @@ heap.insert(41);
 heap.insert(39);
 heap.insert(33);
 heap.insert(18);
-heap.insert(12);
 heap.insert(27);
-heap.insert(123);
+heap.insert(12);
 heap.insert(55);
+
+heap.remove();
 
 console.log(heap);
