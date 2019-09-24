b07636daa50d4ba6e248c340e420fdc4aac032eb
Deepak Kumar <d1612k@gmail.com>
2019-09-24 21:56:52 +0530

    breadth first and depth first graphs

--- graphs.js
+++ graphs.js
@@ -21,6 +21,63 @@ class Graph {
     this.adjacencyList[vertex].forEach(item => this.removeEdge(vertex, item));
     delete this.adjacencyList[vertex];
   }
+
+  depthFirstSearchRecursive(start) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList; // this.adjacencyList won't be accesible inside IIFE
+    (function dfs(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach(neighbor => {
+        if (!visited[neighbor]) return dfs(neighbor);
+      });
+    })(start);
+    return result;
+  }
+
+  depthFirstSearchIterative(start) {
+    const stack = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+
+    visited[start] = true;
+    while (stack.length) {
+      currentVertex = stack.pop();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
+
+  breadthFirstSearch(start) {
+    const queue = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+    visited[start] = true;
+
+    while (queue.length) {
+      currentVertex = queue.shift();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
 }
 
 const graph = new Graph();
@@ -39,6 +96,9 @@ graph.addEdge("Dallas", "Los Angeles");
 graph.addEdge("Hong Kong", "Los Angeles");
 
 // graph.removeEdge("Tokyo", "Dallas");
-graph.removeVertex("Hong Kong");
+// graph.removeVertex("Hong Kong");
 
-console.log(JSON.stringify(graph, undefined, 2));
+// console.log(graph.depthFirstSearchRecursive("Tokyo"));
+// console.log(graph.depthFirstSearchIterative("Tokyo"));
+console.log(graph.breadthFirstSearch("Tokyo"));
+// console.log(JSON.stringify(graph, undefined, 2));
